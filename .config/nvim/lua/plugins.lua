cab2020244dabf5a05a70c7c74b4e2033c4d464e
heisenbug <heisenbug@xyz.com>
2022-12-10 18:49:02 +0800

    *: heisenbug

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -119,12 +119,12 @@ require("lazy").setup({
       end,
     },
 
-    -- {
-    --   "nvim-treesitter/nvim-treesitter", version = false,
-    --   event = { "VeryLazy" },
-    --   opts = { highlight = { enable = true } },
-    --   main = "nvim-treesitter.configs",
-    -- },
+    {
+      "nvim-treesitter/nvim-treesitter", version = false,
+      event = { "VeryLazy" },
+      opts = { highlight = { enable = true } },
+      main = "nvim-treesitter.configs",
+    },
 
     {
       "hrsh7th/nvim-cmp",
